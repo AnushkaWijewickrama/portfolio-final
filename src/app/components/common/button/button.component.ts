761387bf24299ed103a15973003c0610e4780954
Anushka6060 <kushananushka12345@gmail.com>
2025-02-22 09:12:10 +0530

    remove unused files

--- src/app/components/common/button/button.component.ts
+++ src/app/components/common/button/button.component.ts
@@ -4,8 +4,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-button',
   imports: [NgClass],
-  templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  templateUrl: './button.component.html'
 })
 export class ButtonComponent {
   @Input() size: 'lg' | 'sm' = 'sm';
